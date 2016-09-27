c075b48f060a2e53896dd63c9dc065ac0d360aca
roozbeh63 <r.halvaei@gmail.com>
2016-09-27 10:40:15 +0200

    delete not needed files

--- pwmTest/pwmTest.ip_user_files/sim_scripts/design_1/ies/run.f
+++
@@ -1,7 +0,0 @@
--makelib ies/xil_defaultlib \
-  "../../../bd/design_1/ipshared/user.org/axi_pwm_v1_0/sources_1/new/pwmGenerator.vhd" \
-  "../../../bd/design_1/ipshared/user.org/axi_pwm_v1_0/sources_1/new/AXI_PWM.vhd" \
-  "../../../bd/design_1/ip/design_1_AXI_PWM_0_0/sim/design_1_AXI_PWM_0_0.vhd" \
-  "../../../bd/design_1/hdl/design_1.vhd" \
--endlib
-
--- pwmTest/pwmTest.ip_user_files/sim_scripts/design_1/questa/elaborate.do
+++
@@ -1,1 +0,0 @@
-vopt -64 +acc -l elaborate.log -L secureip -L xil_defaultlib -work xil_defaultlib xil_defaultlib.design_1 -o design_1_opt
