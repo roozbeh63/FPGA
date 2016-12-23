15f9c34cf39026dfcf7b86e8f5bf718b93deedc3
roozbeh63 <r.halvaei@gmail.com>
2016-12-23 07:46:59 +0100

    changes on main.c file

--- zybo/main.c
+++ zybo/main.c
@@ -18,39 +18,66 @@
 #define PWM_ENABLE_BASE_ADDR 0x43C10000 + 8
 #define ENCODER_BASE_ADDR 0x43C00000
 
-void *getVirtualAddress(int phys_addr){
-    void *mapped_base;
-    int memfd;
+void *mapped_dev_base;
 
-    void *mapped_dev_base;
+int mmapInit()
+{
+   int memfd;
+   //open file
+   memfd = open("/dev/mem", O_RDWR | O_SYNC);
+   if (memfd ==  -1){
+       printf("can't open /dev/mem\n");
+       exit(0);
+   }
+}
+
+//this function gets physical address of the memory and
+//gives virtual address which is understandable for the OS
+//through this function we can have directly have access to the memory without needing the device drive
+void *getVirtualAddress(int phys_addr, int memfd){
+    void *mapped_base;
+
     off_t dev_base = phys_addr;
 
-    memfd = open("/dev/mem", O_RDWR | O_SYNC);
-    if (memfd ==  -1){
-        printf("can't open /dev/mem\n");
-        exit(0);
-    }
-
+    //read memory
     mapped_base = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, memfd, dev_base & ~MAP_MASK);
     if(mapped_base == (void *) -1){
         printf("can't map the memory to user space.\n");
         exit(0);
     }
     mapped_dev_base =  mapped_base + (dev_base & MAP_MASK);
+
     return mapped_dev_base;
+}
+
+void  mmapExit(int memfd)
+{
+    //close the file
+    if (close (memfd) == -1)
+    {
+        printf("can't close /dev/mem\n");
+        exit(0);
+    }
+    //free the memory
+    if (munmap (mapped_dev_base, MAP_SIZE) == -1) {
+        printf("can't unmap the memory from user space.\n");
+        exit(0);
+    }
+
 }
 
 
 int main(){
-    int *_PWM_period = getVirtualAddress(PWM_PERIOD_BASE_ADDR);
-    int *_duty_cycle = getVirtualAddress(PWM_DUTY_CYCLE_BASE_ADDR);
+    int fd = mmapInit();
+    int *_PWM_period = getVirtualAddress(PWM_PERIOD_BASE_ADDR, fd);
+    int *_duty_cycle = getVirtualAddress(PWM_DUTY_CYCLE_BASE_ADDR, fd);
     //int *_enable = getVirtualAddress(PWM_ENABLE_BASE_ADDR);
     *_PWM_period = 0x186A0;
     *_duty_cycle = 0xC350;
     //*_enable = 0x0;
     //*_duty_cycle = 0x5F5E100;
 
-    int *_encoder = getVirtualAddress(ENCODER_BASE_ADDR);
+    int *_encoder = getVirtualAddress(ENCODER_BASE_ADDR, fd);
 #ifdef DEBUG
     printf("hello zedboard\n");
     printf("pwm%p\n", _PWM_period);
@@ -85,5 +112,6 @@ int main(){
         printf("led1%x\n", *_led1);
         sleep(0.1);
     }*/
+    mmapExit(fd);
     return 0;
 }
